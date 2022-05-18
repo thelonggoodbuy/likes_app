71ace6c271c0604a143b714221fad1e52e1cb7b4
thelonggoodbuy <thelonggoodbuy1991gmail.com>
2022-05-18 13:09:18 +0300

    Maybe Finish

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, request
+import json
 
 app = Flask(__name__)
 
 likes_names = []
-likes_url = ",".join(likes_names)
+
+
+class WordBigerOrHaveBannedCharacters(Exception):
+    def __init__(self, likes_names):
+        super().__init__(likes_names)
+    
+    def validationNames(likes_names):
+        for name in likes_names:
+                if name.isalpha() and len(name)<11:
+                    continue
+                else:
+                    raise WordBigerOrHaveBannedCharacters(likes_names)
+
 
 
 @app.route("/", methods=["POST", "GET"])
 
 def homepage():
     if request.method == "POST":
-        print(request.form['name'])
         likes_names.append(request.form['name'])
 
     return render_template('homepage.html')
@@ -18,41 +30,32 @@ def homepage():
 
 
 @app.route("/likes/")
-# @app.route("/likes")
 def likes():
-    
-    print(likes_names)
+     
+    dict_results = {"error": False, "data": 'None', "error_message": 'None'}
 
-    for name in likes_names:
-        if name.isalpha() and len(name)<11:
-            continue
+    try:
+        WordBigerOrHaveBannedCharacters.validationNames(likes_names)
+
+        upper_names = [name.title() for name in likes_names]
+        tuple_names = tuple(upper_names)
+
+        if len(tuple_names) == 0:
+            data = "Это никоему не нравиться"
+        elif len(tuple_names) == 1:
+            data = "{0} лайкнул это".format(*tuple_names)
+        elif len(tuple_names) == 2:
+            data = "{0} и {1} лайкнули это".format(*tuple_names)
+        elif len(tuple_names) == 3:
+            data = "{0}, {1} и {2} лайкнули это".format(*tuple_names)
         else:
-            data = 'ERROR'
-            return data
+            data = "{0} и {1} и еще {2} лайкнули это".format(tuple_names[0],\
+                tuple_names[1], len(upper_names[2:]))
+        dict_results["data"] = data
+        return dict_results
 
-    upper_names = [name.title() for name in likes_names]
-
-    tuple_names = tuple(upper_names)
-
-    if len(tuple_names) == 0:
-        data = "Это никоему не нравиться"
-    elif len(tuple_names) == 1:
-        data = "{0} лайкнул это".format(*tuple_names)
-    elif len(tuple_names) == 2:
-        data = "{0} и {1} лайкнули это".format(*tuple_names)
-    elif len(tuple_names) == 3:
-        data = "{0}, {1} и {2} лайкнули это".format(*tuple_names)
-    else:
-        data = "{0} и {1} и еще {2} лайкнули это".format(tuple_names[0],\
-            tuple_names[1], len(upper_names[2:]))
-    return data
-
-
-
-
-# 0.5 + 1 + 1.5 +1.5
-
-# 5. - воскресенье
-
-
-# 45min
+    except WordBigerOrHaveBannedCharacters:
+        dict_results["error"] = True
+        dict_results["data"] = 'None'
+        dict_results["error_message"] = '''Вы использовали имя содержащее не буквенные символы или оно больше 10 символов'''
+        return dict_results
